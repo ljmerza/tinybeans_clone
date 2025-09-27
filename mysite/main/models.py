7efc8039df5bdfe47b3ec0c5ac820221284a029c
Leonardo Merza <ljmerza@gmail.com>
2025-09-27 16:56:11 -0400

    t

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,6 @@
 # This file maintains backwards compatibility
 # Models can be organized in the models/ directory as needed
-from .models import *
+# Import all models from the models package when they are created
+# from .models import *
+
+# Currently no models defined in main app
